18b60ba3213695b2d85de5341beec506049338d9
Gerhard Lemmer <ST10253994@vcconnect.edu.za>
2026-02-24 15:09:10 +0200

    feat: add Sidebar and NewArrivalsSection components, integrate tw-merge for utility class merging

--- ShoeHomePage/src/components/Nav.jsx
+++ ShoeHomePage/src/components/Nav.jsx
@@ -1,50 +1,66 @@
-import NikeLogo from "../assets/nike-logo.svg";
+import { TbShoppingBag } from "react-icons/tb";
+import NikeLogo from "../assets/nike-logo.svg?react";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { FaShoppingCart } from "react-icons/fa";
 import { useState } from "react";
 
-const ROUTES = ["Home", "Shop", "About", "Contact"];
+const ROUTES = [
+  "Home",
+  "About",
+  "Services",
+  "Pricing",
+  "Contact",
+];
+export function Nav({ onClickShoppingBtn }) {
+  const [isMobileMenuShown, setIsMobileMenuShown] =
+    useState(false);
+  return (
+    <nav className="relative z-10 flex flex-wrap items-center justify-between">
+      {/* Logo */}
+      <a href="#">
+        <NikeLogo className="h-20 w-20 dark:fill-white" />
+      </a>
 
-export function Nav(){
-    const [isMobileMenuShown , setIsMobileMenuShown ] = useState(false);
-    return  <nav className="z-10 relative flex items-center justify-between">
-        {/*logo*/}
-        <a href="#" className="p-4">
-            <img src={NikeLogo} alt="Nike logo" className="h-20 w-20" />
-        </a>
+      {/* Burger button */}
+      <button
+        onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
+        className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 lg:hidden"
+      >
+        <RxHamburgerMenu size={25} />
+      </button>
 
-        {/*burgerButton*/}
-        <button onClick={() => setIsMobileMenuShown(!isMobileMenuShown)} className="focus:ring-2 focus:ring-gray-200 p-2 rounded-lg hover:bg-gray-100 lg:hidden top-4 right-4">
-            <RxHamburgerMenu size={25} />
-        </button>
+      {/* Menu list */}
+      <div
+        className={`${
+          isMobileMenuShown === false && "hidden"
+        } w-full lg:block lg:w-auto`}
+      >
+        <ul className="flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg lg:flex-row lg:space-x-8 lg:border-none lg:bg-transparent lg:dark:text-white">
+          {ROUTES.map((route, i) => {
+            return (
+              <li
+                className={`cursor-pointer rounded px-3 py-2 lg:hover:bg-transparent lg:hover:text-blue-500 ${
+                  i === 0
+                    ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500 "
+                    : "hover:bg-gray-100"
+                } ${(i == 3 || i == 4) && "lg:text-white"}`}
+                key={route}
+              >
+                <a>{route}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-        {/*navLinks*/}
-        <div className={`${
-            isMobileMenuShown === false && "hidden"
-        }
-        w-full lg:w-auto lg:block`}
-        >
-            <ul className="bg-gray-50 text-lg border-gray-100 rounded-lg p-4 lg:flex lg:justify-center lg:space-x-8 lg:bg-transparent lg:border-none ">
-                {ROUTES.map((route, i) => {
-                    return (
-                    <li  className={`lg:hover:text-blue-500 lg:hover:bg-transparent px-3 py-2 cursor-pointer rounded ${
-                        i === 0
-                         ? "bg-blue-400 text-white lg:bg-transparent lg:text-blue-400" 
-                         : "hover:bg-gray-100"
-                         } ${(i==2 || i==3) && "lg:text-white"}`} 
-                         key={route}>
-                         {route}
-                         </li>
-                    );
-                })}
-            </ul>
+      {/* Cart button */}
+      <div
+        onClick={onClickShoppingBtn}
+        className="btn-press-anim fixed bottom-4 left-4 lg:static lg:mr-8"
+      >
+        <div className="flex-center h-12 w-12 cursor-pointer rounded-full bg-white shadow-md">
+          <TbShoppingBag />
         </div>
-
-        {/*cartButton*/}
-        <div className="fixed left-4 bottom-4 lg:static lg:mr-8 cursor-pointer">
-            <div className="flex items-center justify-center rounded-full h-12 w-12 bg-white shadow-md">
-                <FaShoppingCart size={25} />
-            </div>
-        </div>
+      </div>
     </nav>
+  );
 }
