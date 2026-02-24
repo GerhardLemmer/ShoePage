da8a92fc6e76634174550d07296ffbeb6424300c
Gerhard Lemmer <ST10253994@vcconnect.edu.za>
2026-02-24 15:02:31 +0200

    feat: refactor ShoeDetail component to accept shoe data and implement quantity and size selection

--- ShoeHomePage/src/components/ShoeDetail.jsx
+++ ShoeHomePage/src/components/ShoeDetail.jsx
@@ -1,37 +1,58 @@
-import nike1 from "../assets/n1-min.png";
+import { useState } from "react";
+import { QTY, SIZES } from "../constant";
+import { Select } from "./Select";
 
-export function ShoeDetail(){
-    return (
-        <div className="flex flex-col lg:flex-row-reverse space-y-4 pl-5 pr-5">
-            {/*Shoe image*/}
-            <div className="flex-1 lg:-mt-32">
-                <div className="h-full flex justify-center items-center from- bg-gradient-to-br from-[#F637CF] via-[#E3D876] to-[#4DD4C6]">
-                    <img src={nike1}/>
-                </div>
-            </div>
-            <div className="flex-1 space-y-6">
-                {/*title*/}
-                <div className="text-5xl font-black md:text-9xl">
-                    Nike Air Max 270
-                </div>
-                {/*description*/}
-                <div className="font-medium md:text-xl">
-                    {
-                    "The Nike Air Max 270 features Nike's biggest heel Air unit yet for a super-soft ride that feels as impossible as it looks. The sleek design lines and premium materials are inspired by the iconic Air Max silhouette."
-                    }
-                </div>
-                <div className="text-3xl font-extrabold md:text-6xl">
-                    100$
-                </div>
-                <div className="space-x-10">
-                    <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
-                    Add to bag
-                </button>
-                <a href="#" className="text-lg font-bold hover:underline-offset-4">
-                    See more details
-                </a>
-                </div>
-            </div>
+export function ShoeDetail({ shoe, onClickAdd }) {
+  const [form, setForm] = useState({ qty: null, size: null });
+  return (
+    <div className="flex flex-col space-y-4 dark:text-white lg:flex-row-reverse">
+      {/* Shoe image */}
+      <div className="flex-1 lg:-mt-32 lg:ml-28">
+        <div className="flex-center  h-full bg-gradient-to-br from-[#F637CF] from-5% via-[#E3D876] via-40% to-[#4DD4C6]">
+          <img className="animate-float" src={shoe.src} />
         </div>
-    )
+      </div>
+      <div className="flex-1 space-y-6">
+        {/* Shoe text details */}
+        <div className="text-5xl font-black md:text-9xl">
+          {shoe.title}
+        </div>
+        <div className="font-medium md:text-xl">
+          {shoe.description}
+        </div>
+        <div className="flex space-x-6">
+          <div className=" text-3xl font-extrabold md:text-6xl">
+            {shoe.price} $
+          </div>
+          <Select
+            value={form.qty}
+            onChange={(qty) => setForm({ ...form, qty })}
+            title={"QTY"}
+            options={QTY}
+          />
+          <Select
+            value={form.size}
+            onChange={(size) => setForm({ ...form, size })}
+            title={"SIZE"}
+            options={SIZES}
+          />
+        </div>
+        {/* Shoe buttons and links */}
+        <div className="space-x-10">
+          <button
+            onClick={() => onClickAdd(shoe, form.qty, form.size)}
+            className="btn-press-anim h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700 dark:bg-white  dark:text-black"
+          >
+            Add to bag
+          </button>
+          <a
+            href="#"
+            className="text-lg font-bold underline underline-offset-4"
+          >
+            View details
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
