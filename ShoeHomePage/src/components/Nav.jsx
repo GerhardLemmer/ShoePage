2cc49c95e705222b90377de1cdde9417f350fb79
Gerhard Lemmer <ST10253994@vcconnect.edu.za>
2026-02-24 02:21:11 +0200

    Add navigation component and Nike logo
    - Created Nav component with responsive design including a hamburger menu for mobile view.
    - Integrated Nike logo SVG into the Nav component.
    - Added ShoeDetail component file for future implementation.

---
+++ ShoeHomePage/src/components/Nav.jsx
@@ -0,0 +1,50 @@
+import NikeLogo from "../assets/nike-logo.svg";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
+
+const ROUTES = ["Home", "Shop", "About", "Contact"];
+
+export function Nav(){
+    const [isMobileMenuShown , setIsMobileMenuShown ] = useState(false);
+    return  <nav className="flex items-center justify-between">
+        {/*logo*/}
+        <a href="#">
+            <img src={NikeLogo} alt="Nike logo" className="h-20 w-20" />
+        </a>
+
+        {/*burgerButton*/}
+        <button onClick={() => setIsMobileMenuShown(!isMobileMenuShown)} className="focus:ring-2 focus:ring-gray-200 p-2 rounded-lg hover:bg-gray-100 lg:hidden top-4 right-4">
+            <RxHamburgerMenu size={25} />
+        </button>
+
+        {/*navLinks*/}
+        <div className={`${
+            isMobileMenuShown === false && "hidden"
+        }
+        w-full lg:w-auto lg:block`}
+        >
+            <ul className="bg-gray-50 text-lg border-gray-100 rounded-lg p-4 lg:flex lg:justify-center lg:space-x-8 lg:bg-transparent lg:border-none ">
+                {ROUTES.map((route, i) => {
+                    return (
+                    <li  className={`px-3 py-2 cursor-pointer rounded ${
+                        i === 0
+                         ? "bg-blue-400 text-white lg:bg-transparent lg:text-blue-400" 
+                         : "hover:bg-gray-100"
+                         }`} 
+                         key={route}>
+                         {route}
+                         </li>
+                    );
+                })}
+            </ul>
+        </div>
+
+        {/*cartButton*/}
+        <div className="fixed left-4 bottom-4 lg:static">
+            <div className="flex items-center justify-center rounded-full h-12 w-12 bg-white shadow-md">
+                <FaShoppingCart size={25} />
+            </div>
+        </div>
+    </nav>
+}
